95de434e3b6d69b484b46735b986d805c02d743e
druid0228 <druid0228@naver.com>
2016-11-09 23:05:43 +0900

    wav sample add

--- main_state.py
+++ main_state.py
@@ -119,10 +119,10 @@ def update():
     character.update(frame_time)
     time+=1
     if time%100 == 0:
+        notes.append(Note(random.randint(1,4)))
         #notes.append(Note(random.randint(1,4)))
-        #notes.append(Note(random.randint(1,4)))
-        notes.append(Note(1))
-        notes.append(Note(3))
+        #notes.append(Note(1))
+        #notes.append(Note(3))
     for note in notes:
             #Pass
         if(character.judge.check(note,key)==False):
