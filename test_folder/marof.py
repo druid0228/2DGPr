4b1c730a23282201b552cd41f6e12b846e1797f9
druid0228 <druid0228@naver.com>
2016-11-17 22:56:37 +0900

    marof 테스트

---
+++ test_folder/marof.py
@@ -0,0 +1,37 @@
+from pico2d import *
+import start_state
+
+line_one, line_two, line_three, line_four = 1, 2, 3, 4
+
+
+class Maro:
+    def __init__(self, line):
+        self.x = start_state.scr_width - 50
+        self.image=load_image("note.png")
+
+        if (line == line_one):
+            self.y = start_state.scr_height - 300
+        elif (line == line_two):
+            self.y = start_state.scr_height - 400
+        elif (line == line_three):
+            self.y = start_state.scr_height - 500
+        elif (line == line_four):
+            self.y = start_state.scr_height - 600
+        self.width = 100
+        self.height = 100
+        self.line = line
+        self.mid=self.x+self.width/2
+
+
+    def update(self):
+        if (self.x > 0):
+            self.x -= 1
+            self.mid-=1
+
+    def draw(self):
+        draw_rectangle(self.x, self.y, self.x + self.width, self.y + self.height)
+        self.image.clip_draw(0,0,64,64,self.x+50,self.y+50)
+
+    def __del__(self):
+        del(self.image)
+        pass
